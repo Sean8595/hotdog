0316f898163ff9ac06f8d7315c9ede14c19eb8f1
Sean Melvin <seanmelvin8595@gmail.com>
2019-11-25 20:31:48 -0500

    added connection to server

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
 var Sequelize = require("sequelize");
 
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-var sequelize = new Sequelize("hotdog_db", "root", "seanmelvin8595", {
-  host: "localhost",
+var sequelize = new Sequelize("u7jvolrozed9704e", "qzawwj5tooapigbp", "bn1f4ncqx53godbx", {
+  host: "localjw0ch9vofhcajqg7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	host",
   port: 3306,
   dialect: "mysql",
   pool: {
